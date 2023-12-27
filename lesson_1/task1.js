b262f2ae41e6cf822739f3e65088e7aaa8ed77e2
d.ridniy <allbegj@gmail.com>
2023-12-27 18:55:48 +0500

    first commit

---
+++ lesson_1/task1.js
@@ -0,0 +1,51 @@
+﻿"use strict";
+
+/*
+###Задание 1
+Создайте обычный объект "Музыкальная коллекция", который можно итерировать.
+Каждая итерация должна возвращать следующий альбом из коллекции. Коллекция 
+альбомов - это массив внутри нашего объекта (создать несколько альбомов самому).
+Каждый альбом имеет следующую структуру:
+{
+  title: "Название альбома",
+  artist: "Исполнитель",
+  year: "Год выпуска"
+}
+Используйте цикл for...of для перебора альбомов в музыкальной коллекции и 
+вывода их в консоль в формате:
+"Название альбома - Исполнитель (Год выпуска)"
+*/
+
+
+"use strict";
+
+// Создаем музыкальную коллекцию
+const musicalCollection = {
+  albums: [
+    { title: "The Best of Kirkorov", artist: "Kirkorov", year: "2024" },
+    { title: "Last Christmas", artist: "Wham!", year: "1986" },
+    { title: "99 Luftballons", artist: "Nena", year: "2000" },
+  ],
+
+  // Добавляем метод для итерации по альбомам
+  // Итератор должен возвращать следующий альбом при каждом вызове next()
+  [Symbol.iterator]: function () {
+    let currentIndex = 0;
+    return {
+      next: () => {
+        if (currentIndex < this.albums.length) {
+          const album = this.albums[currentIndex++];
+          return { value: album, done: false };
+        } else {
+          return { done: true };
+        }
+      },
+    };
+  },
+};
+//-----------------------------------------------------------------//
+console.log('//-------Task01-------//')
+// Используем цикл for...of для перебора
+for (const album of musicalCollection) {
+  console.log(`${album.title} - ${album.artist} (${album.year})`);
+}
---
+++ lesson_1/task2.js
@@ -0,0 +1,141 @@
+﻿"use strict";
+
+/*
+###Задание 2
+Вы управляете рестораном, в котором работают разные повара, специализирующиеся 
+на определенных блюдах. Клиенты приходят и делают заказы на разные блюда.
+Необходимо реализовать функцию newOrder. Создавать вспомогательные функции, 
+коллекции, не запрещается. Старайтесь использовать коллекции Map/Set, где это 
+актуально. Представленный ниже код должен работать.
+
+Повара и их специализации:
+Олег - специализация: Пицца.
+Андрей - специализация: Суши.
+Анна - специализация: Десерты.
+
+Блюда, которые могут заказать посетители:
+Пицца "Маргарита"
+Пицца "Пепперони"
+Пицца "Три сыра"
+Суши "Филадельфия"
+Суши "Калифорния"
+Суши "Чизмаки"
+Суши "Сеякемаки"
+Десерт Тирамису
+Десерт Чизкейк
+*/
+
+// Посетитель ресторана.
+class Client {
+  constructor(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+  }
+}
+
+// Вам необходимо реализовать класс, который управляет заказами и поварами.
+
+class Chef {
+  constructor(name, specialization) {
+    this.name = name;
+    this.specialization = specialization;
+  }
+}
+class Manager {
+  constructor() {
+    this.chefs = new Map();
+    this.menu = new Map();
+  }
+
+  addDish(name, type) {
+    this.menu.set(name, type);
+  }
+
+  registerChef(name, specialization) {
+    const chef = new Chef(name, specialization);
+    this.chefs.set(specialization, chef);
+  }
+
+  newOrder(client, ...orders) {
+    console.log(`Клиент ${client.firstname} ${client.lastname} заказал:`);
+    for (const order of orders) {
+      const { name, quantity, type } = order;
+      if (!this.menu.has(name)) {
+        console.error(`Десерт "${name}" - такого блюда не существует.`);
+        continue;
+      }
+
+      const chef = this.chefs.get(this.menu.get(name));
+      console.log(`${type} "${name}" - ${quantity}; готовит повар ${chef.name}`);
+    }
+  }
+}
+
+// Можно передать внутрь конструктора что-либо, если необходимо.
+const manager = new Manager();
+
+// Регистрируем поваров.
+manager.registerChef("Олег", "Пицца");
+manager.registerChef("Андрей", "Суши");
+manager.registerChef("Анна", "Десерт");
+
+// Добавляем блюда в меню.
+manager.addDish("Маргарита", "Пицца");
+manager.addDish("Пепперони", "Пицца");
+manager.addDish("Три сыра", "Пицца");
+manager.addDish("Филадельфия", "Суши");
+manager.addDish("Калифорния", "Суши");
+manager.addDish("Чизмаки", "Суши");
+manager.addDish("Сеякемаки", "Суши");
+manager.addDish("Тирамису", "Десерт");
+manager.addDish("Чизкейк", "Десерт");
+
+
+//-----------------------------------------------------------------//
+console.log('//-------Task02-------//')
+
+// Вызовы ниже должны работать верно, менять их нельзя, удалять тоже.
+manager.newOrder(
+  new Client("Иван", "Иванов"),
+  { name: "Маргарита", quantity: 1, type: "Пицца" },
+  { name: "Пепперони", quantity: 2, type: "Пицца" },
+  { name: "Чизкейк", quantity: 1, type: "Десерт" },
+);
+// Вывод:
+// Клиент Иван заказал: 
+// Пицца "Маргарита" - 1; готовит повар Олег
+// Пицца "Пепперони" - 2; готовит повар Олег
+// Десерт "Чизкейк" - 1; готовит повар Анна
+
+// ---
+
+const clientPavel = new Client("Павел", "Павлов");
+
+manager.newOrder(
+  clientPavel,
+  { name: "Филадельфия", quantity: 5, type: "Суши" },
+  { name: "Калифорния", quantity: 3, type: "Суши" },
+);
+// Вывод:
+// Клиент Павел заказал: 
+// Суши "Филадельфия" - 5; готовит повар Андрей
+// Суши "Калифорния" - 3; готовит повар Андрей
+
+manager.newOrder(
+  clientPavel,
+  { name: "Калифорния", quantity: 1, type: "Суши" },
+  { name: "Тирамису", quantity: 2, type: "Десерт" },
+);
+// Вывод:
+// Клиент Павел заказал: 
+// Суши "Филадельфия" - 5; готовит повар Андрей
+// Суши "Калифорния" - 4; готовит повар Андрей
+// Десерт "Тирамису" - 2; готовит повар Анна
+
+manager.newOrder(
+  clientPavel,
+  { name: "Филадельфия", quantity: 1, type: "Суши" },
+  { name: "Трубочка с вареной сгущенкой", quantity: 1, type: "Десерт" },
+);
+// Ничего не должно быть добавлено, должна быть выброшена ошибка:
+// Десерт "Трубочка с вареной сгущенкой" - такого блюда не существует.
